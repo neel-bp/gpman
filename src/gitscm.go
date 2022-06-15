7bf5ce426be0f96f6bbe4d8114cf1e808dd6a854
neel-bp <neel@bitspro.com>
2022-06-16 00:42:24 +0500

    added decode and decrypt function for git info

--- src/gitscm.go
+++ src/gitscm.go
@@ -25,6 +25,26 @@ func (g *GitInfo) Encode(username, password, url, salt []byte) {
 	g.Salt = base64.StdEncoding.EncodeToString(salt)
 }
 
+func (g GitInfo) Decode() ([]byte, []byte, []byte, []byte, error) {
+	username, err := base64.StdEncoding.DecodeString(g.Username)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	password, err := base64.StdEncoding.DecodeString(g.Password)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	salt, err := base64.StdEncoding.DecodeString(g.Salt)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	url, err := base64.StdEncoding.DecodeString(g.Url)
+	if err != nil {
+		return nil, nil, nil, nil, err
+	}
+	return username, password, url, salt, nil
+}
+
 func (g *GitInfo) Encrypt(passphrase, username, password, url string) error {
 	username_enc, salt, err := Encrypt(passphrase, nil, []byte(username))
 	if err != nil {
@@ -42,6 +62,35 @@ func (g *GitInfo) Encrypt(passphrase, username, password, url string) error {
 	g.Encode(username_enc, password_enc, url_enc, salt)
 
 	return nil
+}
+
+func (g GitInfo) Decrypt(passphrase string) (string, string, string, error) {
+	var username, password, url string
+	username_b, password_b, url_b, salt_b, err := g.Decode()
+	if err != nil {
+		return username, password, url, err
+	}
+
+	username_dec, err := Decrypt(passphrase, salt_b, username_b)
+	if err != nil {
+		return username, password, url, err
+	}
+	username = string(username_dec)
+
+	password_dec, err := Decrypt(passphrase, salt_b, password_b)
+	if err != nil {
+		return username, password, url, err
+	}
+	password = string(password_dec)
+
+	url_dec, err := Decrypt(passphrase, salt_b, url_b)
+	if err != nil {
+		return username, password, url, err
+	}
+	url = string(url_dec)
+
+	return username, password, url, nil
+
 }
 
 func GitAuthInit(passphrase, url, username, access_token string) error {
